bb76d758f3a7bdd47af85408be10f7dd7068b7af
sunJ0120 <sspure1214@gmail.com>
2025-08-31 23:35:33 +0900

    Feat : findOne 관련 test 추가 및 통과완료

--- emotionDiary/be/src/test/java/sunj/be/domain/diary/DiaryRepositoryTest.java
+++ emotionDiary/be/src/test/java/sunj/be/domain/diary/DiaryRepositoryTest.java
@@ -12,6 +12,7 @@ import sunj.be.domain.user.UserRepository;
 import sunj.be.dto.DiaryDto;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -20,7 +21,9 @@ class DiaryRepositoryTest {
     DiaryRepository diaryRepository;
     @Autowired
     UserRepository userRepository;
+
     private Long userId;
+    private Long diaryId;
 
     @BeforeEach
     public void init(){
@@ -30,6 +33,7 @@ class DiaryRepositoryTest {
         user.writeDiary("오늘 기분 좋은!", Emotion.GOOD); //연관관계 편의 메서드로 저장
         userRepository.save(user); //영속 컨텍스트에 반영 및 DB에 플러시
         this.userId = user.getId(); //save 후에는 userId가 채워 지므로 가능하다.
+        this.diaryId = user.getDiaryList().get(0).getId();
     }
 
     @Test
@@ -42,5 +46,16 @@ class DiaryRepositoryTest {
         //then
         Assertions.assertEquals(1, dtos.size());
         Assertions.assertEquals("GOOD", dtos.get(0).emotion());
+    }
+
+    @Test
+    void findByIdAndUserIdTest() throws Exception {
+        //given
+
+        //when
+        Optional<DiaryDto> dto = diaryRepository.findByIdAndUserId(diaryId,userId);
+
+        //then
+        Assertions.assertEquals("GOOD", dto.get().emotion());
     }
 }
